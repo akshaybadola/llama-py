c364dd91241d7eedb61f806ba4ea32b98acf443b
Akshay <akshay.badola.cs@gmail.com>
2025-08-08 04:06:03 +0530

    Added configurable port to proxy

--- proxy.py
+++ proxy.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 import yaml
 import uvicorn
@@ -11,7 +12,8 @@ if __name__ == "__main__":
 
     async def run_proxy(config):
         app = model_manager_app(config)  # starts proc automatically
-        uvicorn_config = uvicorn.Config(app=app, host="0.0.0.0", port=8000, log_level="debug")
+        uvicorn_config = uvicorn.Config(app=app, host="0.0.0.0", port=int(sys.argv[1]),
+                                        log_level="debug")
         server = uvicorn.Server(uvicorn_config)
         await server.serve()
     asyncio.run(run_proxy(config))
